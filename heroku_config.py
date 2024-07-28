d1a57266d9c5387e028435c8b61c07b4178c3bee
Aditya <79218915+Paramatin-OP@users.noreply.github.com>
2024-07-29 00:21:41 +0530

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Var(object):
-    APP_ID = int(os.environ.get("APP_ID", 6))
+    APP_ID = int(os.environ.get("APP_ID", "1848509"))
     # 6 is a placeholder
-    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    API_HASH = os.environ.get("API_HASH", "dd51a36a6bb43c9b9b2824f3bbdcc797")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOLcBuyOLdqm5M4NIBVLJnSgUDnIhbwgAlfZsk2Z5P0JqwV-JPAP7Yr5mMmSeoZgIcnCagE9mr4tRm_jYK9Jm9ESbQzHajO2Iwrg1YB-Ufoa9UxYYS87nV7tgNCvxtFWAy84PuYDZH-US-w8d1AXiWQtz2zr3qhKSCsAvbiw4vP0V5BVJKn52t8b7LniT8B4uWlMmHhrqYmX9NYH-uVcL5K0k5zyuChC7b68ZBmpimzOkCEEkB3GnUT9HBdx__P0TKI0wYYIhVegYM1Lo4Gte1kijFyybqcoS5qX9raVNdCAN0S26H6jYh5oWxbaZbElLgYV0rs9w0ewe4y1zz5sw2UxNJ_M=")
     DB_URI = os.environ.get("DATABASE_URL", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", None)
     LOGGER = True
@@ -19,9 +19,9 @@ class Var(object):
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
     # Send .get_id in any channel to fill this value.
-    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", -100))
-    PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID", -100))
-    PM_PERMIT_GROUP_ID = os.environ.get("PM_PERMIT_GROUP_ID", None)
+    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", "-4282837820"))
+    PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID", "-4282837820"))
+    PM_PERMIT_GROUP_ID = os.environ.get("PM_PERMIT_GROUP_ID", "-4282837820")
     TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
     NO_SONGS = bool(os.environ.get("NO_SONGS", False))
     DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
@@ -36,7 +36,7 @@ class Var(object):
         t_file = open(TEMP_DOWNLOAD_DIRECTORY + "auth_token.txt", "w")
         t_file.write(AUTH_TOKEN_DATA)
         t_file.close()
-    PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
+    PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", "-4282837820")
     if PRIVATE_GROUP_ID is not None:
         try:
             PRIVATE_GROUP_ID = int(PRIVATE_GROUP_ID)
