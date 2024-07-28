e8523b6f6b56c063fa001efda2961c825a419661
Aditya <79218915+Paramatin-OP@users.noreply.github.com>
2024-07-29 03:30:39 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -24,8 +24,7 @@ telever = "5.0"
 apiid = os.environ.get('APP_ID')
 api_hash = os.environ.get('API_HASH')
 session_name = os.environ.get('STRING_SESSION')
-if not apiid or not api_hash:
-    raise ValueError("API ID or API Hash is missing. Make sure they are set as environment variables.")
+
 api_id = int(apiid)
 # Initialize the TelegramClient
 bot = TelegramClient(session_name, api_id, api_hash)
