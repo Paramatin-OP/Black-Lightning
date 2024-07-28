b9449c86525075c4ebc5706ed6eec7c4ad128af6
Aditya <79218915+Paramatin-OP@users.noreply.github.com>
2024-07-29 02:45:47 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -21,18 +21,25 @@ from .function import thunderfunction as topfunc
 
 StartTime = time.time()
 telever = "5.0"
+api_id = os.environ.get('APP_ID')
+api_hash = os.environ.get('API_HASH')
 
+if not api_id or not api_hash:
+    raise ValueError("API ID or API Hash is missing. Make sure they are set as environment variables.")
+
+# Initialize the TelegramClient
+bot = TelegramClient('session_name', api_id, api_hash)
 Lastupdate = time.time()
 sed = logging.getLogger("WARNING")
 sedprint = logging.getLogger("WARNING")
 CMD_HNDLR = os.environ.get("CMD_HNDLR", "\.")
 
 if Var.STRING_SESSION:
-    session_name = str("1BVtsOLcBuyOLdqm5M4NIBVLJnSgUDnIhbwgAlfZsk2Z5P0JqwV-JPAP7Yr5mMmSeoZgIcnCagE9mr4tRm_jYK9Jm9ESbQzHajO2Iwrg1YB-Ufoa9UxYYS87nV7tgNCvxtFWAy84PuYDZH-US-w8d1AXiWQtz2zr3qhKSCsAvbiw4vP0V5BVJKn52t8b7LniT8B4uWlMmHhrqYmX9NYH-uVcL5K0k5zyuChC7b68ZBmpimzOkCEEkB3GnUT9HBdx__P0TKI0wYYIhVegYM1Lo4Gte1kijFyybqcoS5qX9raVNdCAN0S26H6jYh5oWxbaZbElLgYV0rs9w0ewe4y1zz5sw2UxNJ_M=")
-    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+    session_name = str(Var.STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), 'api_id', 'api_hash')
 else:
     session_name = "startup"
-    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+    bot = TelegramClient(session_name, 'api_id', 'api_hash')
 
 
 
